c768bb64baeaf71a64aed055cd38ffc1518a6828
riyanza2330 <riyan.armajaya3008@gmail.com>
2026-01-03 14:31:35 +0700

    script.js

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ function download() {
   const url = document.getElementById("url").value;
   const type = document.getElementById("type").value;
   const status = document.getElementById("status");
+  const progress = document.getElementById("progress");
+  const bar = document.querySelector(".bar");
 
   if (!url) {
-    status.innerText = "❌ Link kosong bro";
+    status.innerText = "❌ Masukkan URL";
     return;
   }
 
-  status.innerText = "⏳ Processing...";
+  progress.classList.remove("hidden");
+  bar.style.width = "0%";
+
+  let percent = 0;
+  const loading = setInterval(() => {
+    percent += 10;
+    bar.style.width = percent + "%";
+    if (percent >= 100) clearInterval(loading);
+  }, 200);
+
+  saveHistory(url, type);
 
   const api = `https://downloader-api.onrender.com/${type}?url=${encodeURIComponent(url)}`;
+  window.open(api, "_blank");
+
+  status.innerText = "✅ Download diproses";
+}
